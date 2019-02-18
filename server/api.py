13ed568d263342ecceec9784430ab53e04096e93
David Stevens <dstevens@kth.se>
2019-02-18 12:20:07 +0100

    Add support for GET at /events

--- server/api.py
+++ server/api.py
@@ -1,22 +1,20 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import inspect
+import time
 
 from database import db_session
-from models import User
+from models import User, Friends, Player, FantasyDraft, FantasyLeague, FantasyResult, Tournament, Event, VideoGame, FantasyDraft
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
 class Users(Resource):
     def get(self):
-        users = User.query.all()
+        users = User.query.limit(10).all()
         return {'users': [x.as_dict() for x in users]}
-    
+
     def put(self):
         parser = reqparse.RequestParser()
         for arg, datatype in User.constructor_params():
@@ -26,12 +24,29 @@ class Users(Resource):
         db_session.add(User(**args))
         db_session.commit()
 
-api.add_resource(HelloWorld, '/')
+
+class Events(Resource):
+    def get(self):
+        events = Event.query.filter(Event.start_at > time.time()).limit(10)
+        tournament_ids = [e.tournament_id for e in events]
+        tournaments = Tournament.query.filter(
+            Tournament.tournament_id.in_(tournament_ids)).all()
+        print(tournaments)
+        print(events)
+        print({e.tournament_id for e in events})
+        return {'tournaments': [{**t.as_dict(), 'events': [
+            e.as_dict() for e in events if e.tournament_id == t.tournament_id]}
+            for t in tournaments]}
+
+
 api.add_resource(Users, '/users')
+api.add_resource(Events, '/events')
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
