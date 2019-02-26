aef4fe5adb7a09dcf2f87ea27fe5ddd10ce6f565
David Stevens <dstevens@kth.se>
2019-02-26 12:28:16 +0100

    Download and handle images for tournaments from SmashGG

--- server/smashgg.py
+++ server/smashgg.py
@@ -2,6 +2,8 @@ import requests
 from database import db_session
 from models import Event, Tournament
 from time import time
+import os
+from io import open as iopen
 
 
 class SmashGG:
@@ -36,6 +38,9 @@ class SmashGG:
                     id
                     name
                     slug
+                    images {
+                        url
+                    }
                     events {
                         id
                         name
@@ -59,8 +64,30 @@ class SmashGG:
                               json={'query': gql_query, 'variables': gql_vars},
                               headers={"Authorization": f"Bearer {self.api_key}"})
         for tournament in r.json()['data']['tournaments']['nodes']:
+            image_dir = f"images/tournaments/{tournament['id']}"
+            if not os.path.exists(image_dir):
+                os.mkdir(image_dir)
+            images = tournament['images']
+            icon_path = image_dir + '/icon.png'
+            if not os.path.exists(icon_path):
+                icon = requests.get(images[0])
+                if icon.status_code == requests.codes.ok:
+                    with iopen(icon_path) as file:
+                        file.write(icon.content)
+                else:
+                    banner_path = None
+            banner_path = image_dir + '/banner.png'
+            if not os.path.exists(banner_path):
+                banner = requests.get(images[1])
+                if banner.status_code == requests.codes.ok:
+                    with iopen(banner_path) as file:
+                        file.write(banner.content)
+                else:
+                    banner_path = None
             t = Tournament(tournament['id'],
-                           tournament['name'], tournament['slug'])
+                           tournament['name'], tournament['slug'], 
+                           icon_path=os.path.abspath(icon_path), 
+                           banner_path=os.path.abspath(banner_path))
             db_session.add(t)
             for event in tournament['events']:
                 e = Event(event['id'], event['name'], t.tournament_id,
