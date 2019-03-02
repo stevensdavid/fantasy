10585feb432c5236e2b4c2907956b83457bb0d71
David Stevens <dstevens@kth.se>
2019-03-02 17:47:49 +0100

    Add potential idea for SSL

--- server/api_server/__init__.py
+++ server/api_server/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+# from flask_talisman import Talisman
 import os
 
 # Initialize endpoints and database connection
 app = Flask(__name__)
 api = Api(app)
+# Wrap flask for SSL
+# Talisman(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://fantasy:dB13%bofLUM1*sNG3%%p@dstevens.se/fantasy'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
