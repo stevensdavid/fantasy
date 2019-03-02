85c6d9a12af453979fb2a3d4f56b90378dc1d1d0
David Stevens <dstevens@kth.se>
2019-03-02 13:43:19 +0100

    Add image dir to config

--- server/api_server/__init__.py
+++ server/api_server/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import os
 
 # Initialize endpoints and database connection
 app = Flask(__name__)
@@ -9,3 +10,4 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://fantasy:dB13%bofLUM1*sNG3%%p@dstevens.se/fantasy'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+app.config['IMAGE_DIR'] = os.getcwd() + '/images'
