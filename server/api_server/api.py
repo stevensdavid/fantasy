bc67e4541a7cea37753075a515a6f0b10bcf017f
David Stevens <dstevens@kth.se>
2019-03-02 14:19:22 +0100

    Implement endpoints for images and featured tournaments

--- server/api_server/api.py
+++ server/api_server/api.py
@@ -2,10 +2,12 @@ import inspect
 import time
 from datetime import date
 
-from flask import Flask
+from flask import Flask, send_from_directory, send_file,  make_response, safe_join
 from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
+import os
+import io
 
 from . import api, app, db
 from .marshmallow_schemas import (ConstantsSchema, EventSchema,
@@ -25,11 +27,21 @@ event_schema = EventSchema()
 fantasy_draft_schema = FantasyDraftSchema()
 fantasy_league_schema = FantasyLeagueSchema()
 fantasy_result_schema = FantasyResultSchema()
-friends_schema = FriendsSchema()
+friend_schema = FriendsSchema()
 player_schema = PlayerSchema()
 tournament_schema = TournamentSchema()
 user_schema = UserSchema()
 video_game_schema = VideoGameSchema()
+
+events_schema = EventSchema(many=True)
+fantasy_drafts_schema = FantasyDraftSchema(many=True)
+fantasy_leagues_schema = FantasyLeagueSchema(many=True)
+fantasy_results_schema = FantasyResultSchema(many=True)
+friends_schema = FriendsSchema(many=True)
+players_schema = PlayerSchema(many=True)
+tournaments_schema = TournamentSchema(many=True)
+users_schema = UserSchema(many=True)
+video_games_schema = VideoGameSchema(many=True)
 
 """
 API endpoints providing support for:
@@ -61,18 +73,24 @@ class UsersAPI(Resource):
         parser = make_pagination_reqparser()
         parser.add_argument('gamertag', str)
         args = parser.parse_args(strict=True)
-        users = User.query.filter(User.tag.like(f'%{args["gamertag"]}%')).paginate(
-            page=args['page'], per_page=args['perPage']).items
-        return {'users': [x.as_dict() for x in users]}
+        print(args['page'])
+        users = User.query.all()
+        print([x.as_dict() for x in users])
+        # users = User.query.filter(User.tag.like(f'%{args["gamertag"]}%')).paginate(
+        #     page=args['page'], per_page=args['perPage']).items
+        return user_schema.jsonify(users)
 
-    def put(self):
+    def post(self):
         parser = reqparse.RequestParser()
-        for arg, datatype in User.constructor_params():
-            parser.add_argument(arg, type=datatype)
+        # for arg, datatype in User.constructor_params():
+        #     parser.add_argument(arg, type=datatype)
+        parser.add_argument('tag', type=str)
+        parser.add_argument('email', type=str)
+        parser.add_argument('pw', str)
         args = parser.parse_args(strict=True)
         # pylint: disable=no-member
-        db.add(User(**args))
-        db.commit()
+        db.session.add(User(**args))
+        db.session.commit()
 
 
 class EventsAPI(Resource):
@@ -101,18 +119,18 @@ class FriendsAPI(Resource):
     def put(self):
         args = self._parse_put_delete()
         # Create symmetrical entities
-        db.add(Friends(args['id'], args['friendId']))
-        db.add(Friends(args['friendId'], args['id']))
-        db.commit()
+        db.session.add(Friends(args['id'], args['friendId']))
+        db.session.add(Friends(args['friendId'], args['id']))
+        db.session.commit()
         # Return no-content.
         # TODO: consider returning new object, probably better design
         return '', 204
 
     def delete(self):
         args = self._parse_put_delete()
-        db.delete(Friends(args['id'], args['friendId']))
-        db.delete(Friends(args['friendId'], args['id']))
-        db.commit()
+        db.session.delete(Friends(args['id'], args['friendId']))
+        db.session.delete(Friends(args['friendId'], args['id']))
+        db.session.commit()
         # Return no-content.
         # TODO: consider returning deleted object, probably better design
         return '', 204
@@ -137,26 +155,43 @@ class DatabaseVersionAPI(Resource):
         if req_date > current_version.last_event_update:
             smashgg.get_new_events()
             current_version.last_event_update = date.today()
-            db.commit()
+            db.session.commit()
         return {'last_event_update': current_version.last_event_update}
+
+
+class FeaturedTournaments(Resource):
+    def get(self):
+        tournaments = Tournament.query.filter(
+            Tournament.is_featured & (Tournament.ends_at > time.time())).all()
+        print([x.as_dict() for x in tournaments])
+        return tournaments_schema.jsonify(tournaments)
+
+
+class Images(Resource):
+    def get(self, fname):
+        with open(safe_join(app.config['IMAGE_DIR'], fname), 'rb') as img:
+            return send_file(io.BytesIO(img.read()),
+            mimetype='image/png', as_attachment=True, attachment_filename=os.path.split(fname)[1])
 
 
 api.add_resource(DatabaseVersionAPI, '/event_version')
 api.add_resource(UsersAPI, '/users')
 api.add_resource(EventsAPI, '/events')
 api.add_resource(FriendsAPI, '/friends')
+api.add_resource(FeaturedTournaments, '/featured')
+api.add_resource(Images, '/images/<path:fname>')
 
 
 def make_pagination_reqparser():
-    parser = reqparse.RequestParser()
-    parser.add_argument('page', int)
-    parser.add_argument('perPage', int)
+    parser = reqparse.RequestParser(bundle_errors=True)
+    parser.add_argument('page', type=int)
+    parser.add_argument('perPage', type=int)
     return parser
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
-    db.remove()
+    db.session.remove()
 
 
 def main():
