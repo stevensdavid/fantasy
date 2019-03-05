bbde60f8eecf51bccdb3b59ba643bd1b297d8c55
Ruwaid Louis <ruwaid@kth.se>
2019-03-05 19:07:36 +0100

    Added search feauture

--- client/components/Tournament/TournamentSearch.js
+++ client/components/Tournament/TournamentSearch.js
@@ -1,48 +1,58 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView, FlatList, Text, Image } from 'react-native';
+import { StyleSheet, View, ScrollView, FlatList, Text, Image, Alert } from 'react-native';
 import { SearchBar, Card } from 'react-native-elements';
 
 
 export class TournamentSearch extends React.Component {
+    searchTournament(term) {
+        fetch(global.server + '/tournaments' + (term !='' ? ('?name=' + term) : ''), {
+            method: "GET",
+            headers: this.httpGetHeaders
+        })
+        .then((response) => {
+            if(response.status === 404 || response.status === 400) {
+                Alert.alert("Alert", "(404 or 400) Should not be seing this.");
+              } else if (response.status === 200) {
+                response.json().then((respjson) => {
+                  this.state.data = [];
+                  respjson.map((tournamentInfo) => {
+                      this.state.data.push({
+                          key: tournamentInfo.tournament_id,
+                          img_uri: (tournamentInfo.ext_icon_url != null ? tournamentInfo.ext_icon_url : ''),
+                          title: tournamentInfo.name
+                      });
+                  })
+                })
+              }
+        })
+        .catch((error) => {
+            console.error('Fetch featured error: ' + error);
+        });
+    }
+
     constructor(props){
         super(props);
     
         this.state = { 
           search: '',
           data: [
-            {key: '1', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D 3D 3D 3D 3D 3D 3D 3D 3D'}, 
-            {key: '2', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '3', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '4', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '5', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '6', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '7', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '8', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '9', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '10', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '11', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '12', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '13', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '14', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '15', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '16', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '17', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '18', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '19', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '20', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '21', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '22', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}, 
-            {key: '23', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}]
-        };     
+            {key: '1', img_uri: 'https://smashgg.imgix.net/images/tournament/110416/image-face35c5085dd11074f02063bb8d8d58.png', title:'The Big Deal: 3D'}]
+        };
+
+        this.searchTournament('');
     }
 
-    render() {
+    httpGetHeaders = {};
 
+    render() {
         return (
             <View>
             <SearchBar
                 placeholder="Search"
-                onChangeText={(text) => this.setState({search: text})}
+                onChangeText={(text) => {
+                    this.setState({search: text});
+                    this.searchTournament(this.state.search);
+                }}
                 value={this.state.search}
                 containerStyle={styles.searchContainer}
                 inputContainerStyle={styles.searchInputContainer}
@@ -58,8 +68,8 @@ export class TournamentSearch extends React.Component {
                         <Image
                             resizeMode="cover"
                             style={styles.image}
-                            source={{uri: item.img_uri}}/>                       
-                        <Text style={styles.headerText}>{item.title}</Text>
+                            source={{uri: item.img_uri}}/>
+                        <Text style={styles.headerText}>{item.title}</Text>                     
                     </View>
                     </Card>}
             />
@@ -100,6 +110,7 @@ const styles = StyleSheet.create({
         alignSelf:'center',
         justifyContent:'center',
         alignItems:'center',
+        marginLeft: 10,
         marginRight: 70,
         fontSize: 21,
         fontWeight: 'bold',
