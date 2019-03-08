a2399363dd2b2d65ac54de3510f514042bdd0b7b
David Stevens <dstevens@kth.se>
2019-03-08 19:54:45 +0100

    Add inputs

--- client/components/League/CreateLeagueForm.js
+++ client/components/League/CreateLeagueForm.js
@@ -11,7 +11,7 @@ import {
     Alert,
     Platform
 } from 'react-native';
-  
+
 export class CreateLeagueForm extends React.Component {
     constructor(props) {
         super(props)
@@ -19,7 +19,7 @@ export class CreateLeagueForm extends React.Component {
             eventId: props.eventId,
             isSnake: false,
             name: '',
-            public: false,
+            isPublic: false,
             draftSize: 5
         }
     }
@@ -28,14 +28,98 @@ export class CreateLeagueForm extends React.Component {
         return (
             <View style={styles.container}>
                 <View style={styles.inputContainer}>
+                    <Text>League name</Text>
                     <TextInput style={styles.inputs}
-                        placeholder="League name"
+                        placeholder="Press to type..."
                         keyboardType="default"
                         underlineColorAndroid='transparent'
-                        onChangeText={(name) => this.setState({name})}
+                        onChangeText={(name) => this.setState({ name })}
                     />
                 </View>
+                <View style={styles.inputContainer}>
+                    <Text>Draft size</Text>
+                    <TextInput style={styles.inputs}
+                        defaultValue="5"
+                        keyboardType="numeric"
+                        underlineColorAndroid='transparent'
+                        onChangeText={(draftSize) => this.setState({ draftSize })}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text>Snake draft</Text>
+                    <Switch style={styles.inputs}
+                        onValueChange={(isSnake) => this.setState({ isSnake })}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text>Public</Text>
+                    <Switch style={styles.inputs}
+                        onValueChange={(isPublic) => this.setState({ isPublic })}
+                    />
+                </View>
+
+                <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('Edit')}>
+                    <Text style={styles.loginText}>Create</Text>
+                </TouchableHighlight>
             </View>
-        )
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 120,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+    },
+    inputContainer: {
+        borderBottomColor: '#F5FCFF',
+        backgroundColor: '#FFFFFF',
+        borderRadius: 30,
+        borderBottomWidth: 1,
+        width: 250,
+        height: 45,
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    spinnerTextStyle: {
+        color: '#FFF',
+    },
+    inputs: {
+        height: 45,
+        marginLeft: 16,
+        borderBottomColor: '#FFFFFF',
+    },
+    inputIcon: {
+        width: 30,
+        height: 30,
+        marginLeft: 15,
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 250,
+        borderRadius: 30,
+    },
+    iconContainer: {
+        height: 72,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 250,
+        borderRadius: 30,
+    },
+    loginButton: {
+        backgroundColor: "#b3002d",
+    },
+    loginText: {
+        color: 'white',
+    }
+});
