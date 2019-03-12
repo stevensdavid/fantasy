42480ff7216c56f7a7f4095af93c4a5fefaa69fa
Ruwaid Louis <ruwaid@kth.se>
2019-03-12 17:32:11 +0100

    Add optional SearchBar which filters list on title

--- client/components/Container/ScrollableListContainer.js
+++ client/components/Container/ScrollableListContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ActivityIndicator, StyleSheet, View, FlatList, Text, Image, TouchableOpacity } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, SearchBar } from 'react-native-elements';
+import { HideAbleView } from '../View/HideAbleView';
 
 /*Takes the following props:
     data: Array with objects each containing {key, img_uri(optional), title, description(optional)}
@@ -10,40 +11,83 @@ import { Card } from 'react-native-elements';
 
 export class ScrollableListContainer extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = {
+            show: this.props.data,
+            search: ''
+        }
+
+        this.dataHolder = this.props.data;
+
+        this.searchFilterFunction = this.searchFilterFunction.bind(this);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.data != this.props.data) {
+            this.setState({
+                show: this.props.data
+            });
+            this.dataHolder = this.props.data
+        }
+    }
+
+    searchFilterFunction(text) {
+        this.setState({ search: text });
+        const newData = this.dataHolder.filter(item => {
+            const itemData = `${item.title.toUpperCase()}`;
+
+            const textData = text.toUpperCase();
+
+            return itemData.indexOf(textData) > -1;
+        });
+
+        this.setState({ show: newData });
     }
 
     render() {
         return (
-        (this.props.loading? (
-            <View style={{flex: 1, justifyContent: 'center'}}>
-            <ActivityIndicator animating={this.props.loading} size="large" color="#b3002d" />
-            </View>
-        ): (
-        <FlatList
-            data={this.props.data}
-            renderItem={({item}) => 
-                <TouchableOpacity onPress={() =>  {this.props.onItemClick ? this.props.onItemClick(item.key) : {}}}>
-                <Card containerStyle={styles.cardContainer}>
-                <View style={{flexDirection: 'row'}}>
-                    {item.img_uri ? (
-                        <Image
-                        resizeMode="cover"
-                        style={styles.image}
-                        source={{uri: item.img_uri}}/>
-                    ) : (
-                        <View></View>
-                    )}
-                    <View>
-                    <Text style={styles.headerText}>{item.title}</Text>
-                    <Text style={styles.descriptionText}>{item.description ? item.description : ''}</Text>
-                    </View>                    
+            (this.props.loading ? (
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <ActivityIndicator animating={this.props.loading} size="large" color="#b3002d" />
                 </View>
-                </Card>
-                </TouchableOpacity>}
-        />
-        )))
+            ) : (
+                    <View>
+                        <HideAbleView hide={this.props.showSearchBar ? !this.props.showSearchBar : true}>
+                            <SearchBar
+                                placeholder="Search"
+                                onChangeText={text => this.searchFilterFunction(text)}
+                                containerStyle={styles.searchContainer}
+                                value={this.state.search}
+                                inputContainerStyle={styles.searchInputContainer}
+                                inputStyle={styles.searchInput}
+                                placeholderTextColor="#b3002d"
+                            />
+                        </HideAbleView>
+                        <FlatList
+                            data={this.state.show}
+                            renderItem={({ item }) =>
+                                <TouchableOpacity onPress={() => { this.props.onItemClick ? this.props.onItemClick(item.key) : {} }}>
+                                    <Card containerStyle={styles.cardContainer}>
+                                        <View style={{ flexDirection: 'row' }}>
+                                            {item.img_uri ? (
+                                                <Image
+                                                    resizeMode="cover"
+                                                    style={styles.image}
+                                                    source={{ uri: item.img_uri }} />
+                                            ) : (
+                                                    <View></View>
+                                                )}
+                                            <View>
+                                                <Text style={styles.headerText}>{item.title}</Text>
+                                                <Text style={styles.descriptionText}>{item.description ? item.description : ''}</Text>
+                                            </View>
+                                        </View>
+                                    </Card>
+                                </TouchableOpacity>}
+                        />
+                    </View>
+                )))
     }
 }
 
@@ -56,7 +100,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
     },
     image: {
-        width: 90, 
+        width: 90,
         height: 90,
     },
     headerText: {
@@ -70,5 +114,17 @@ const styles = StyleSheet.create({
         marginRight: 70,
         fontSize: 12,
         fontWeight: 'bold',
-    }
+    },
+    searchContainer: {
+        backgroundColor: 'transparent',
+        borderBottomColor: 'transparent',
+        borderTopColor: 'transparent',
+    },
+    searchInputContainer: {
+        backgroundColor: 'transparent',
+        borderRadius: 20
+    },
+    searchInput: {
+        color: '#b3002d',
+    },
 });
