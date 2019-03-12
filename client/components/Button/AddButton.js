2d83d627b8d5bc4558b2421647002aff2ac9746b
Ruwaid Louis <ruwaid@kth.se>
2019-03-12 13:49:16 +0100

    Remove margin styling

--- client/components/Button/AddButton.js
+++ client/components/Button/AddButton.js
@@ -37,8 +37,6 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        marginBottom: 15,
-        marginRight: 27,
         width: 50,
         borderRadius: 50
     },
