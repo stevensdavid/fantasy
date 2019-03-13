1e533e7fa03ee5b1c1eb037549ddceb6af5902fa
Ruwaid Louis <ruwaid@kth.se>
2019-03-13 22:14:19 +0100

    Change name

--- client/components/Profile/FriendsView.js
+++ client/components/Profile/FriendsView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Icon } from 'expo';
 
-export default class TabBarIcon extends React.Component {
+export default class FriendsView extends React.Component {
   render() {
     return (
       <Text>Hi</Text>
