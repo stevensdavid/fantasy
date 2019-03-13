9472151358c69c7907e06954ead3580aee36f6de
Ruwaid Louis <ruwaid@kth.se>
2019-03-13 22:12:10 +0100

    Init FriendView

---
+++ client/components/Profile/FriendView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Icon } from 'expo';
+
+export default class TabBarIcon extends React.Component {
+  render() {
+    return (
+      <Text>Hi</Text>
+    );
+  }
+}
