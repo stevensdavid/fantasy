d903161c6c28cf84235747065c21191c166d9e9c
David Stevens <dstevens@kth.se>
2019-03-13 23:55:11 +0100

    Fix check for if draft has started

--- server/api_server/api.py
+++ server/api_server/api.py
@@ -1173,7 +1173,7 @@ class FantasyResultAPI(Resource):
             league_id=args['leagueId']).first()
         if not league:
             return {'error': 'League not found'}, 400
-        if league.is_snake and league.fantasy_results:
+        if league.is_snake and league.fantasy_drafts:
             return {'error': 'Cannot join an in-progress snake draft'}, 400
         if ((not user_is_logged_in(league.owner_id)
              and not user_is_logged_in(args['userId']))
