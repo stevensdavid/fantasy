a5ec950a62f4778412663cac22d881157df85ca2
David Stevens <dstevens@kth.se>
2019-03-14 15:06:11 +0100

    Add support for uploading profile photos

--- client/components/Profile/ProfileView.js
+++ client/components/Profile/ProfileView.js
@@ -8,6 +8,7 @@ import {
   Alert,
   TouchableOpacity
 } from "react-native";
+import { ImagePicker, Permissions } from 'expo';
 import { Icon } from "react-native-elements";
 import Spinner from "react-native-loading-spinner-overlay";
 
@@ -89,6 +90,46 @@ export class ProfileView extends React.Component {
       });
   }
 
+  async pickAndUploadPhoto() {
+    // Heavily inspired by https://stackoverflow.com/a/42521680
+    const { status: cameraRollPerm } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+    if (cameraRollPerm !== 'granted') {
+      return;
+    }
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 0.1
+    });
+    if (result.cancelled) {
+      return;
+    }
+    let localUri = result.uri;
+    let filename = localUri.split('/').pop();
+    // Infer the type of the image
+    let match = /\.(\w+)$/.exec(filename);
+    let type = match ? `image/${match[1]}` : `image`;
+    console.log(type);
+    if (!["image/jpeg", "image/jpg", "image/png"].includes(type.toLowerCase())) {
+      Alert.alert('Unsupported file type, please choose a JPG, JPEG or PNG image');
+      return;
+    }
+
+    // Upload the image using the fetch and FormData APIs
+    let formData = new FormData();
+    // Assume "photo" is the name of the form field the server expects
+    formData.append('photo', { uri: localUri, name: filename, type });
+
+    return await fetch(global.server + '/images/' + global.userID, {
+      method: 'POST',
+      body: formData,
+      header: {
+        'content-type': 'multipart/form-data',
+      },
+    });
+  }
+
   render() {
     const rightArrow = (
       <Icon
@@ -115,7 +156,8 @@ export class ProfileView extends React.Component {
           textStyle={styles.spinnerTextStyle}
         />
         <View style={styles.headerContent}>
-          <Icon name="portrait" type="material" color="black" size={104} />
+          <Icon name="portrait" type="material" color="black" size={104}
+            onPress={this.pickAndUploadPhoto} />
           <View style={{ marginRight: 80 }}>
             <Text
               style={{
