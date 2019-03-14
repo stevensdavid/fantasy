2902d77f77063a7889d5cbcacdc6f3917f43292f
David Stevens <davidstevens.nz@gmail.com>
2019-03-14 11:13:39 +0000

    Fix single-item tuple syntax

--- server/api_server/marshmallow_schemas.py
+++ server/api_server/marshmallow_schemas.py
@@ -12,7 +12,7 @@ class ConstantsSchema(ma.ModelSchema):
 class UserSchema(ma.ModelSchema):
     class Meta:
         model = User
-        exclude = ('pw')
+        exclude = ('pw',)
     fantasy_drafts = ma.Nested(
         'FantasyDraftSchema',
         only=["league_id", "player_id"],
