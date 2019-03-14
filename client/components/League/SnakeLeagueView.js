259f538b6757855c6513cf5cf82e5a2eb91b666b
Ruwaid Louis <ruwaid@kth.se>
2019-03-14 17:39:35 +0100

    Init SnakeLeagueView

---
+++ client/components/League/SnakeLeagueView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export default class SnakeLeagueView extends React.Component {
+  render() {
+    return (
+      <View></View>
+    );
+  }
+}
