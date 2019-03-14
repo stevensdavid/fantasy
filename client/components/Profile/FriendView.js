6bf1849b44b8f363fda0d786ef72793cd13ae2ae
Ruwaid Louis <ruwaid@kth.se>
2019-03-14 10:24:59 +0100

    Implement entire FriendView

---
+++ client/components/Profile/FriendView.js
@@ -0,0 +1,209 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    Image,
+    Alert,
+    TouchableOpacity
+  } from 'react-native';
+import { Icon } from 'react-native-elements';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+export default class FriendsView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      email: '',
+      firstName: '',
+      lastName: '',
+      tag: '',
+      nFriends: 0,
+      nLeagues: 0,
+      tagFontSize: 42,
+      loading: false,
+    }
+
+    this.reloadInfo = this.reloadInfo.bind(this);
+
+    this.friendsID = this.props.navigation.getParam("friendID", -1);
+  }
+
+  reloadInfo() {
+    this.getFriendInfo(this.friendsID);
+  } 
+
+  componentDidMount() {
+    this.getFriendInfo(this.friendsID);
+  }
+
+  httpGetHeaders = {};
+
+  getFriendInfo(id) {
+    this.setState({loading: true});
+    fetch(global.server + "/users/" + id, {
+      method: 'GET',
+      headers: this.httpGetHeaders
+    }).then((response) => {
+      if(response.status === 404 || response.status === 400) {
+        Alert.alert(
+          'ERROR!',
+          'USER OR PAGE NOT FOUND, SHOULD NOT BE SEING THIS!',
+          [
+            {text: 'OK', onPress: () => this.setState({loading: false})}
+          ],
+          { cancelable: false }
+        )
+      } else if(response.status === 200) {
+        response.json().then((responseJSON) => {
+          console.log(responseJSON);
+          this.setState({
+            email: responseJSON.email,
+            firstName: responseJSON.first_name,
+            lastName: responseJSON.last_name,
+            tag: responseJSON.tag,
+            tagFontSize: (38 * 8) / responseJSON.tag.length,
+            nFriends: responseJSON.following.length,
+            nLeagues: responseJSON.fantasy_leagues.length
+          });
+          this.setState({loading: false});
+        })
+      }
+    }).catch((error) => {
+      console.error('GET user error: ' + error);
+      this.setState({loading: false});
+    });
+  }
+
+  render() {
+
+    return (
+      <View>
+      <Spinner visible={this.state.loading} textContent={'Loading...'} textStyle={styles.spinnerTextStyle}/>
+      <View style={styles.headerContent}>
+        <Icon name= 'portrait' type='material' 
+              color='black' size={104}/>
+        <View style={{marginRight: 80}}>
+        <Text style={{fontWeight: 'bold', marginLeft: 4, fontSize: this.state.tagFontSize}}
+            underlineColorAndroid='transparent'>
+            #{this.state.tag}
+        </Text>
+        <Text style={({fontSize: 14, color:'#b3002d'})}>Score: 0</Text>
+        </View>
+      </View>
+      <View style={styles.container}>
+
+              <View style={styles.textContainer}>
+                <Text style={styles.inputs}
+                    secureTextEntry={true}
+                    underlineColorAndroid='transparent'>
+                    {this.state.firstName}
+                </Text>
+              </View>
+
+              <View style={styles.textContainer}>
+                <Text style={styles.inputs}
+                    secureTextEntry={true}
+                    underlineColorAndroid='transparent'>
+                    {this.state.lastName}
+                </Text>
+              </View>
+
+              <View style={styles.textContainer}>
+                <Text style={styles.inputs}
+                    secureTextEntry={true}
+                    underlineColorAndroid='transparent'>
+                    {this.state.email}
+                </Text>
+              </View>
+              
+              <TouchableOpacity>
+                <View style={styles.linkContainer}>
+                  <Text style={[styles.inputs, {fontWeight: "bold"}]}
+                      underlineColorAndroid='transparent'>
+                      Friends: {this.state.nFriends}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity>
+                <View style={styles.linkContainer}>
+                  <Text style={[styles.inputs, {fontWeight: "bold"}]}
+                      underlineColorAndroid='transparent'>
+                      Leagues: {this.state.nLeagues}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+          </View>
+        </View>
+    );
+  }
+}
+
+styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerContent: {
+    margin: 35,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  linkContainer: {
+    zIndex: 1,
+    height:45,
+    flexDirection: 'row',
+    marginBottom:20,
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius:30,
+    borderBottomWidth: 1,
+    width:250,
+  },
+  spinnerTextStyle: {
+    color: '#FFF',
+  },
+  inputs:{
+    fontSize: 26,
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    zIndex: 1,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    marginRight: 10,
+    width:100,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "#b3002d",
+  },
+  loginText: {
+    color: 'white',
+  }
+}); 
