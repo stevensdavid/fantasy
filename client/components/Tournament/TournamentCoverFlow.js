2c33d4b8b01f975aa5b6f54db5ee9e9ed2aab7b1
Ruwaid Louis <ruwaid@kth.se>
2019-03-15 15:40:51 +0100

    Fix bug in class code

--- client/components/Tournament/TournamentCoverFlow.js
+++ client/components/Tournament/TournamentCoverFlow.js
@@ -1,106 +1,125 @@
-import React from 'react';
-import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Carousel from "react-native-carousel-control";
-import { Card} from 'react-native-elements';
-import {TournamentView} from '../Tournament/TournamentView';
+import { Card } from "react-native-elements";
 
 const styles = StyleSheet.create({
   bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30
   },
   container: {
-    borderRadius: 10,
+    borderRadius: 10
   },
   image: {
-    width: 270, 
+    width: 270,
     height: 270,
-    marginTop: 10, 
+    marginTop: 10,
     marginBottom: 10
   },
   loadingGIF: {
-    width: 340, 
+    width: 340,
     height: 250,
-    marginTop: 10, 
-    marginBottom: 10,
+    marginTop: 10,
+    marginBottom: 10
   }
 });
 
-let httpGetHeaders = {};
-
-var featuredCards = [];
-let loadingGIF = <Image
-  resizeMode="cover"
-  style={styles.loadingGIF}
-  source={{uri: 'http://www.certainty3d.com/images/loader.gif'}}
-  />
-
-{/*backgroundColor: "#131862"*/}
 export class TournamentCoverFlow extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.fetchPoint = this.fetchPoint.bind(this);
+    this.getFetchPoint = this.getFetchPoint.bind(this);
     this.setFeaturedCards = this.setFeaturedCards.bind(this);
 
     this.mounted = false;
 
     this.state = {
-      showLoading: true,
-    }
+      showLoading: true
+    };
 
-    this.headerFontSize= 21;
+    this.httpGetHeaders = {};
+    this.featuredCards = [];
+    this.loadingGIF = (
+      <Image
+        resizeMode="cover"
+        style={styles.loadingGIF}
+        source={{ uri: "http://www.certainty3d.com/images/loader.gif" }}
+      />
+    );
+
+    this.headerFontSize = 21;
   }
 
-  fetchPoint(callback) {
+  getFetchPoint(callback) {
     fetch(global.server + this.props.fetchPoint, {
-        method: "GET",
-        headers: httpGetHeaders
+      method: "GET",
+      headers: this.httpGetHeaders
+    })
+      .then(response => {
+        return response.json();
       })
-      .then((response) => {return response.json()})
-      .then((responseJson) => {
+      .then(responseJson => {
         callback(responseJson);
       })
-      .catch((error) => {
-        console.error('Fetch error: ' + error);
+      .catch(error => {
+        console.error("Fetch error: " + error);
       });
   }
 
   setFeaturedCards(featuredJSON) {
-    if(this.mounted === false) {
+    if (this.mounted === false) {
       return;
     }
-  featuredJSON.map( tournamentInfo => {
-      featuredCards.push([
-      <View key={tournamentInfo.tournament_id}>
-      <TouchableOpacity onPress={() => this.props.navigation.navigate("Tournament", {tournamentID: tournamentInfo.tournament_id})}>
-      <Card containerStyle={styles.container}>
-      <View>
-      <Image
-        resizeMode="cover"
-        style={styles.image}
-        source={{uri: tournamentInfo.ext_icon_url ? tournamentInfo.ext_icon_url :'https://media1.tenor.com/images/556e9ff845b7dd0c62dcdbbb00babb4b/tenor.gif'}}
-      />
-      <Text style={{alignContent: 'center', textAlign: 'center', color: '#000000', 
-      fontSize: (this.headerFontSize * 20 / tournamentInfo.name.length)}}>{tournamentInfo.name}</Text>
-      </View>
-      </Card>
-      </TouchableOpacity>
-      </View>
-      ])
-    })
-    if(this.mounted === true) {
+    featuredJSON.map(tournamentInfo => {
+      this.featuredCards.push([
+        <View key={tournamentInfo.tournament_id}>
+          <TouchableOpacity
+            onPress={() =>
+              this.props.navigation.navigate("Tournament", {
+                tournamentID: tournamentInfo.tournament_id
+              })
+            }
+          >
+            <Card containerStyle={styles.container}>
+              <View>
+                <Image
+                  resizeMode="cover"
+                  style={styles.image}
+                  source={{
+                    uri: tournamentInfo.ext_icon_url
+                      ? tournamentInfo.ext_icon_url
+                      : "https://media1.tenor.com/images/556e9ff845b7dd0c62dcdbbb00babb4b/tenor.gif"
+                  }}
+                />
+                <Text
+                  style={{
+                    alignContent: "center",
+                    textAlign: "center",
+                    color: "#000000",
+                    fontSize:
+                      Math.min((this.headerFontSize * 20) / tournamentInfo.name.length, 50)
+                  }}
+                >
+                  {tournamentInfo.name}
+                </Text>
+              </View>
+            </Card>
+          </TouchableOpacity>
+        </View>
+      ]);
+    });
+    if (this.mounted === true) {
       this.setState({
         showLoading: false
       });
     }
-    
   }
 
   componentDidMount() {
     this.mounted = true;
-    this.fetchPoint(this.setFeaturedCards);
+    this.getFetchPoint(this.setFeaturedCards);
   }
 
   componentWillMount() {
@@ -109,8 +128,9 @@ export class TournamentCoverFlow extends React.Component {
 
   render() {
     return (
-      <Carousel sneak={40} pageStyle={ {borderRadius: 20}}>
-      {this.state.showLoading ? loadingGIF : featuredCards}
-      </Carousel>)
+      <Carousel sneak={40} pageStyle={{ borderRadius: 20 }}>
+        {this.state.showLoading ? this.loadingGIF : this.featuredCards}
+      </Carousel>
+    );
   }
 }
