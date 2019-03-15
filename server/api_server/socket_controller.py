c7e7ee3dc79ee9c960723f0d0bbf51ce0ae4c4ea
David Stevens <davidstevens.nz@gmail.com>
2019-03-15 10:56:43 +0000

    Track which users are currently in a room

--- server/api_server/socket_controller.py
+++ server/api_server/socket_controller.py
@@ -1,7 +1,10 @@
 from .models import User, FantasyResult
-from .api import user_is_logged_in, user_schema
+from .api import user_is_logged_in, user_schema, users_schema
 from flask_socketio import join_room, leave_room, send, emit
+from flask import request
 from . import socketio
+
+ROOM_MEMBERS = {}
 
 
 @socketio.on('join', namespace='/leagues')
@@ -14,6 +17,11 @@ def join_league_room(msg):
         # The user is a part of the league they are trying to join
         user = User.query.filter_by(user_id=user_id).first()
         join_room(league_id, namespace='/leagues')
+        if not league_id in ROOM_MEMBERS:
+            ROOM_MEMBERS[league_id] = set()
+        ROOM_MEMBERS[league_id].add(user)
+        emit('connected', users_schema.dump(ROOM_MEMBERS[league_id]), 
+             namespace='/leagues', room=request.sid)
         emit('joined-room', user_schema.dump(user),
              namespace='/leagues', room=league_id) 
 
@@ -26,5 +34,10 @@ def leave_league_room(msg):
     league_id = msg['leagueID']
     user = User.query.filter_by(user_id=user_id).first()
     leave_room(league_id)
+    if league_id in ROOM_MEMBERS:
+        try:
+            ROOM_MEMBERS[league_id].remove(user)
+        except KeyError:
+            pass
     emit('left-room', user_schema.dump(user),
          namespace='/leagues', emit=league_id)
