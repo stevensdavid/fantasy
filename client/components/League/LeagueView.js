0c00a3f7aeabf29a3cb01a1166b7bc33890860de
Ruwaid Louis <ruwaid@kth.se>
2019-03-15 17:03:07 +0100

    Fix edit button now showing in leagueview for android

--- client/components/League/LeagueView.js
+++ client/components/League/LeagueView.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
-import { Icon } from 'react-native-elements';
+import { Icon } from "react-native-elements";
 import { ScrollableListContainer } from "../Container/ScrollableListContainer";
-import { HideAbleView } from '../View/HideAbleView';
+import { HideAbleView } from "../View/HideAbleView";
 import { AddButton } from "../Button/AddButton";
 
 export default class LeagueView extends React.Component {
@@ -24,27 +24,31 @@ export default class LeagueView extends React.Component {
       return;
     }
     this.setState({ loading: false });
-    fetch(global.server + '/leagues/' + this.leagueID).then(res => res.json())
+    fetch(global.server + "/leagues/" + this.leagueID)
+      .then(res => res.json())
       .then(league_obj => {
-        this.setState({ league: league_obj })
+        this.setState({ league: league_obj });
         let participants = this.state.league.fantasy_results.reduce(
-          (newObj, x) => Object.assign(
-            newObj, { [x.user.user_id]: { tag: x.user.tag, draft: [] } }
-          ), {}
-        )
+          (newObj, x) =>
+            Object.assign(newObj, {
+              [x.user.user_id]: { tag: x.user.tag, draft: [] }
+            }),
+          {}
+        );
         for (draft of this.state.league.fantasy_drafts) {
-          participants[draft.user_id].draft.push(draft.player.tag)
+          participants[draft.user_id].draft.push(draft.player.tag);
         }
         const newData = Object.keys(participants).map(k => {
           return {
             key: k.toString(),
             title: participants[k].tag,
             description: participants[k].draft.join("\n")
-          }
-        })
-        this.setState({ data: newData })
-        this.setState({ loading: false })
-      }).catch(err => console.error(err));
+          };
+        });
+        this.setState({ data: newData });
+        this.setState({ loading: false });
+      })
+      .catch(err => console.error(err));
 
     this.subs = [
       this.props.navigation.addListener("didFocus", payload => {
@@ -62,48 +66,47 @@ export default class LeagueView extends React.Component {
 
   handlePress(userID) {
     if (userID == global.userID) {
-      this.props.navigation.navigate("EditDraft", {league: this.state.league})
+      this.props.navigation.navigate("EditDraft", {
+        league: this.state.league
+      });
     }
   }
 
   render() {
     return (
-      <View style={{ minHeight: "100%" }}>
-        <Text style={{ alignSelf: "center", fontSize: 32, fontWeight: "bold" }}>
-          {this.state.league.name}
-        </Text>
-        <ScrollableListContainer
-          loading={this.state.loading}
-          data={this.state.data}
-          onItemClick={(userID) => this.handlePress(userID)}
-        />
-        <AddButton hide={this.state.league.owner != global.userID || this.state.loading}
-          buttonName = "edit"
-          containerStyle={styles.floatingButtonStyle}
-          onPress={() => this.props.navigation.navigate("LeagueParticipants", {leagueID: this.leagueID})} />
+      <View>
+        <View style={{ minHeight: "100%" }}>
+          <Text
+            style={{ alignSelf: "center", fontSize: 32, fontWeight: "bold" }}
+          >
+            {this.state.league.name}
+          </Text>
+          <ScrollableListContainer
+            loading={this.state.loading}
+            data={this.state.data}
+            onItemClick={userID => this.handlePress(userID)}
+          />
+        </View>
+        <View>
+          <AddButton
+            hide={
+              this.state.league.owner != global.userID || this.state.loading
+            }
+            buttonName="edit"
+            containerStyle={styles.floatingButtonStyle}
+            onPress={() =>
+              this.props.navigation.navigate("LeagueParticipants", {
+                leagueID: this.leagueID
+              })
+            }
+          />
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    height: 50,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 15,
-    marginRight: 27,
-    width: 50,
-    borderRadius: 50
-  },
-  addButton: {
-    backgroundColor: "#b3002d"
-  },
-  mainContainerStyle: {
-    flexDirection: "column",
-    flex: 1
-  },
   floatingButtonStyle: {
     alignSelf: "flex-end",
     position: "absolute",
--- client/components/League/SnakeLeagueView.js
+++ client/components/League/SnakeLeagueView.js
@@ -14,7 +14,7 @@ export default class SnakeLeagueView extends React.Component {
       data: [],
       turn: -1,
       done: false,
-      isMounted: false,
+      isMounted: false
     };
 
     this.leagueID = this.props.navigation.getParam("leagueID", -1);
@@ -23,7 +23,6 @@ export default class SnakeLeagueView extends React.Component {
       leagueID: this.leagueID,
       token: global.token
     };
-
 
     this.fetchLeagueInfo = this.fetchLeagueInfo.bind(this);
     this.joinedRoom = this.joinedRoom.bind(this);
@@ -34,104 +33,133 @@ export default class SnakeLeagueView extends React.Component {
   }
 
   connected(users) {
-    if(!this.state.isMounted) {return}
+    if (!this.state.isMounted) {
+      return;
+    }
     users.map(x => this.joinedRoom(x));
   }
 
   leftRoom(userID) {
     console.log(userID + " left the room");
-    if(!this.state.isMounted) {return}
+    if (!this.state.isMounted) {
+      return;
+    }
     this.setState({
-      data: this.state.data.map(x => x.key  == userID ? 
-        Object.assign(x, {titleStyle: {color: "gray"}})
-        : x)
+      data: this.state.data.map(x =>
+        x.key == userID
+          ? Object.assign(x, { titleStyle: { color: "gray" } })
+          : x
+      )
     });
   }
 
   joinedRoom(userID) {
     console.log(userID + " joined the room");
-    if(!this.state.isMounted) {return}
+    if (!this.state.isMounted) {
+      return;
+    }
     this.setState({
-      data: this.state.data.map(x => x.key  == userID ? 
-        Object.assign(x, {titleStyle: {color: "black"}})
-        : x)
+      data: this.state.data.map(x =>
+        x.key == userID
+          ? Object.assign(x, { titleStyle: { color: "black" } })
+          : x
+      )
     });
   }
 
   turnChange(userID) {
-    if(userID == null) {
+    if (userID == null) {
       this.setState({
-        data: this.state.data.map(x => Object.assign(x, {status: ""}))
+        data: this.state.data.map(x => Object.assign(x, { status: "" }))
       });
-      this.setState({done: true});
+      this.setState({ done: true });
       return;
     }
     console.log(userID);
     this.setState({
       turn: userID
     });
-    if(!this.state.isMounted) {return}
+    if (!this.state.isMounted) {
+      return;
+    }
     this.setState({
-      data: this.state.data.map(x => x.key  == userID? 
-        Object.assign(x, {status: "[DRAFTER]"})
-        : Object.assign(x, {status: ""}))
+      data: this.state.data.map(x =>
+        x.key == userID
+          ? Object.assign(x, { status: "[DRAFTER]" })
+          : Object.assign(x, { status: "" })
+      )
     });
   }
 
   newDraft(draft) {
     console.log("New draft!");
-    if(!this.state.isMounted) { console.log("returning"); return}
+    if (!this.state.isMounted) {
+      console.log("returning");
+      return;
+    }
     this.setState({
-      data: this.state.data.map(x => x.key  == draft.user.user_id ? 
-        Object.assign(x, {description: x.description + draft.player.tag + "\n"})
-        : x)
+      data: this.state.data.map(x =>
+        x.key == draft.user.user_id
+          ? Object.assign(x, {
+              description: x.description + draft.player.tag + "\n"
+            })
+          : x
+      )
     });
     this.setState({
-      league: Object.assign(this.state.league, {fantasy_drafts: this.state.league.fantasy_drafts.concat(draft)})
-    })
+      league: Object.assign(this.state.league, {
+        fantasy_drafts: this.state.league.fantasy_drafts.concat(draft)
+      })
+    });
   }
 
   handlePress(userID) {
-    if (userID == global.userID && this.state.turn == global.userID){
-      this.props.navigation.navigate("EditDraft", {league: this.state.league});
+    if (userID == global.userID && this.state.turn == global.userID) {
+      this.props.navigation.navigate("EditDraft", {
+        league: this.state.league
+      });
     } else {
-      if(this.state.league.turn == null) {
-        Alert.alert('Drafting is over');
+      if (this.state.league.turn == null) {
+        Alert.alert("Drafting is over");
       } else {
-        Alert.alert('It is not your turn');
+        Alert.alert("It is not your turn");
       }
-      
     }
   }
 
   componentDidMount() {
-    this.setState({isMounted: true}, () => {
-      this.fetchLeagueInfo(this.leagueID).then(() => {
-        this.socket = SocketIOClient(global.server + "/leagues", {
-          secure: true,
-          reconnect: true,
-          transports: ["websocket"]
-        });
-        this.socket.on("connected", this.connected);
-        this.socket.on("left-room", this.leftRoom);
-        this.socket.on("joined-room", this.joinedRoom);
-        this.socket.on("turn-change", this.turnChange);
-        this.socket.on("new-draft", this.newDraft);
-    
-        this.socket.connect();
-        this.socket.emit("join", this.socketIdentifier);
-      // server should answer with joined-room
-      }).catch(err => console.error(err));   
+    this.setState({ isMounted: true }, () => {
+      this.fetchLeagueInfo(this.leagueID)
+        .then(() => {
+          this.socket = SocketIOClient(global.server + "/leagues", {
+            secure: true,
+            reconnect: true,
+            transports: ["websocket"]
+          });
+          this.socket.on("connected", this.connected);
+          this.socket.on("left-room", this.leftRoom);
+          this.socket.on("joined-room", this.joinedRoom);
+          this.socket.on("turn-change", this.turnChange);
+          this.socket.on("new-draft", this.newDraft);
+
+          this.socket.connect();
+          this.socket.emit("join", this.socketIdentifier);
+          // server should answer with joined-room
+        })
+        .catch(err => console.error(err));
     });
   }
 
   componentWillUnmount() {
-    this.setState({isMounted: false});
+    this.setState({ isMounted: false });
     this.socket.emit("leave", this.socketIdentifier);
   }
 
   async fetchLeagueInfo(leagueID) {
-    if(!this.state.isMounted) { console.log("return"); return}
+    if (!this.state.isMounted) {
+      console.log("return");
+      return;
+    }
     let league_obj = {};
     try {
       let res = await fetch(global.server + "/leagues/" + leagueID);
@@ -157,34 +185,50 @@ export default class SnakeLeagueView extends React.Component {
         title: participants[k].tag,
         titleStyle: { color: "gray" },
         status: k == league_obj.turn ? "[DRAFTER]" : "",
-        description: participants[k].draft ? participants[k].draft.join("\n") + "\n" : ''
+        description: participants[k].draft
+          ? participants[k].draft.join("\n") + "\n"
+          : ""
       };
     });
-    this.setState({ data: newData, done: league_obj.turn == null, loading: false });
+    this.setState({
+      data: newData,
+      done: league_obj.turn == null,
+      loading: false
+    });
     this.setState({ turn: this.state.league.turn });
   }
 
   render() {
     return (
-      <View style={{ minHeight: "100%" }}>
-        <Text style={{ alignSelf: "center", fontSize: 32, fontWeight: "bold" }}>
-          {this.state.league.name}
-        </Text>
-        <ScrollableListContainer
-          loading={this.state.loading}
-          data={this.state.data}
-          onItemClick={userID => this.handlePress(userID)}
-        />
-        <AddButton
-          hide={this.state.league.owner != global.userID || this.state.loading || this.state.league.turn == null}
-          buttonName="edit"
-          containerStyle={styles.floatingButtonStyle}
-          onPress={() =>
-            this.props.navigation.navigate("LeagueParticipants", {
-              leagueID: this.leagueID
-            })
-          }
-        />
+      <View>
+        <View style={{ minHeight: "100%" }}>
+          <Text
+            style={{ alignSelf: "center", fontSize: 32, fontWeight: "bold" }}
+          >
+            {this.state.league.name}
+          </Text>
+          <ScrollableListContainer
+            loading={this.state.loading}
+            data={this.state.data}
+            onItemClick={userID => this.handlePress(userID)}
+          />
+        </View>
+        <View>
+          <AddButton
+            hide={
+              this.state.league.owner != global.userID ||
+              this.state.loading ||
+              this.state.league.turn == null
+            }
+            buttonName="edit"
+            containerStyle={styles.floatingButtonStyle}
+            onPress={() =>
+              this.props.navigation.navigate("LeagueParticipants", {
+                leagueID: this.leagueID
+              })
+            }
+          />
+        </View>
       </View>
     );
   }
