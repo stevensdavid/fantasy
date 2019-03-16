7b7aff0898bca11be88eb97dbddd8b9d42b71c88
David Stevens <dstevens@kth.se>
2019-03-16 14:54:35 +0100

    Track connected users' sockets

--- server/api_server/socket_controller.py
+++ server/api_server/socket_controller.py
@@ -5,24 +5,40 @@ from flask import request
 from . import socketio
 
 ROOM_MEMBERS = {}
+SOCKETS = {}
+
+
+@socketio.on('connect', namespace='/')
+def user_connected(msg):
+    user_id = int(msg['userID'])
+    if user_is_logged_in(user_id, msg['token']):
+        SOCKETS[user_id] = request.sid
+
+
+@socketio.on('disconnect', namespace='/')
+def user_disconnected():
+    for k, v in SOCKETS.items():
+        if v == request.sid:
+            del SOCKETS[k]
 
 
 @socketio.on('join', namespace='/leagues')
 def join_league_room(msg):
     user_id = int(msg['userID'])
     league_id = int(msg['leagueID'])
-    if (user_is_logged_in(user_id, msg['token']) and 
+    if (user_is_logged_in(user_id, msg['token']) and
         FantasyResult.query.filter_by(league_id=league_id,
                                       user_id=user_id).first()):
         # The user is a part of the league they are trying to join
         join_room(league_id, namespace='/leagues', sid=request.sid)
-        if not league_id in ROOM_MEMBERS:
+        if league_id not in ROOM_MEMBERS:
             ROOM_MEMBERS[league_id] = set()
         ROOM_MEMBERS[league_id].add(user_id)
-        emit('connected',list(ROOM_MEMBERS[league_id]), 
+        emit('connected', list(ROOM_MEMBERS[league_id]),
              namespace='/leagues', room=request.sid)
         emit('joined-room', user_id,
-             namespace='/leagues', room=league_id) 
+             namespace='/leagues', room=league_id)
+
 
 @socketio.on('leave', namespace='/leagues')
 def leave_league_room(msg):
